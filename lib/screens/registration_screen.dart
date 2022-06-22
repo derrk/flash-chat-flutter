1f1ac5f93897d5277948331076ce7cc2ca43b09c
Derrk <derrik.pollock@gmail.com>
2022-06-22 00:06:56 -0500

    Restarted project from stub projet, attempting to resolve issues w/ Firebase config

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -4,17 +4,21 @@ import 'package:flash_chat/components/rounded_button.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'registration_screen';
+
+  const RegistrationScreen({Key key}) : super(key: key);
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email, password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -26,31 +30,38 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 child: Image.asset('images/logo.png'),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 48.0,
             ),
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+                email = value;
               },
               decoration:
                   kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
             ),
-            SizedBox(
+            const SizedBox(
               height: 8.0,
             ),
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
               decoration: kTextFieldDecoration.copyWith(
                   hintText: 'Enter your password'),
             ),
-            SizedBox(
+            const SizedBox(
               height: 24.0,
             ),
             RoundedButton(
-                title: 'Register', color: Colors.blueAccent, onPressed: () {}),
+                title: 'Register',
+                color: Colors.blueAccent,
+                onPressed: () {
+                  print(email);
+                  print(password);
+                }),
           ],
         ),
       ),
